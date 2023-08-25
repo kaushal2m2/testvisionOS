56c8aac3228604d76630c9a6aad282df4e70511e
Kaushal Marimuthu <109944592+kaushal2m2@users.noreply.github.com>
2023-08-24 19:29:54 -0700

    Initial Commit

---
+++ Test VisionOS/Test_VisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_VisionOSApp.swift
+//  Test VisionOS
+//
+//  Created by Kaushal Marimuthu on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_VisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
